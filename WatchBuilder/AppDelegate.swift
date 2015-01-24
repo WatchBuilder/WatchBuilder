df4f1a8e6f16331040376b55a4224a5d6667a609
William Kent <wjk011@gmail.com>
2015-01-24 18:34:02 -0500

    Configure main-menu nib

--- WatchBuilder/AppDelegate.swift
+++ WatchBuilder/AppDelegate.swift
@@ -10,18 +10,11 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(notification: NSNotification) {
         // Insert code here to initialize your application
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
+    
+    func applicationWillTerminate(notification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
