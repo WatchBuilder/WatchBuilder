36a6518a4caaae132f0e78b36794853744600fdd
William Kent <wjk011@gmail.com>
2015-01-26 18:24:37 -0500

    Add JSONEditor framework

---
+++ JSONEditor/JSONEditor.h
@@ -0,0 +1,12 @@
+//
+//  JSONEditor.h
+//  JSONEditor
+//
+//  Created by William Kent on 1/26/15.
+//  Copyright (c) 2015 William Kent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT double JSONEditorVersionNumber;
+FOUNDATION_EXPORT const unsigned char JSONEditorVersionString[];
---
+++ JSONEditor/JSONEditorController.swift
@@ -0,0 +1,16 @@
+//
+//  JSONEditorController.swift
+//  WatchBuilder
+//
+//  Created by William Kent on 1/26/15.
+//  Copyright (c) 2015 William Kent. All rights reserved.
+//
+
+import Cocoa
+
+class JSONEditorController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+}
