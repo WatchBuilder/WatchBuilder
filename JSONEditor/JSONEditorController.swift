1f458eabacf20c088030b21cc2ecd28d090dec65
William Kent <wjk011@gmail.com>
2015-01-26 21:36:46 -0500

    Add JSONValueContainer

--- JSONEditor/JSONEditorController.swift
+++ JSONEditor/JSONEditorController.swift
@@ -9,6 +9,18 @@
 import Cocoa
 import SwiftyJSON
 
+private class JSONValueContainer: NSObject {
+    // Note: This variable must be declared this way,
+    // or else the Swift compiler crashes (as of build 6C107a).
+    private var value = JSON([:])
+    
+    private init(value: JSON) {
+        self.value = value
+    }
+}
+
+// MARK: -
+
 public class JSONEditorController: NSViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
